887e88a196b30916602ad096eb865ac391ca9a40
Louis JZ <louis.ljz08@gmail.com>
2023-11-09 20:08:47 +0700

    display msg

--- Home.py
+++ Home.py
@@ -6,7 +6,8 @@ if "vector_store" not in st.session_state:
 
 st.set_page_config('Home', '📖')
 st.title('VectaServe HomePage 📖')
-st.info('Please verify your credentials to enable the Customer Service bot to retrieve information from your VectaraDB.')
+msg = st.empty()
+msg.info('Please verify your credentials to enable the Customer Service bot to retrieve information from your VectaraDB.')
 
 customer_id = st.text_input('Customer ID')
 corpus_id = st.text_input('Corpus ID')
@@ -14,3 +15,4 @@ api_key = st.text_input('API Key')
 
 if customer_id and corpus_id and api_key:
     st.session_state.vector_store = Vectara(customer_id, corpus_id, api_key)
+    msg.success('Connection to VectaraDB Established!')
