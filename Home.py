5f419618038919eac4beccf5fa0c02e0c5f6701e
Louis JZ <louis.ljz08@gmail.com>
2023-11-09 17:56:11 +0700

    finalize webapp

--- Home.py
+++ Home.py
@@ -5,7 +5,8 @@ if "vector_store" not in st.session_state:
     st.session_state.vector_store = None
 
 st.set_page_config('Home', '📖')
-st.title('Docusphere HomePage 📖')
+st.title('VectaServe HomePage 📖')
+st.info('Please verify your credentials to enable the Customer Service bot to retrieve information from your VectaraDB.')
 
 customer_id = st.text_input('Customer ID')
 corpus_id = st.text_input('Corpus ID')
@@ -13,4 +14,3 @@ api_key = st.text_input('API Key')
 
 if customer_id and corpus_id and api_key:
     st.session_state.vector_store = Vectara(customer_id, corpus_id, api_key)
-st.info('Ensure you have entered the correct credentials, or else the CS bot cannot retrieve any information.')
